e75beb6a964401b50c19549098927f80db0e9eeb
SamanR86 <Sadr@727>
2022-08-23 13:45:10 +0430

    Add project files.

---
+++ fibonacci.web/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fibonacci.web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Messeage { get; set; }
+        
+        public void OnGet()
+        {
+            int number = 0;
+            Fiboo fiboonachi   =new Fiboo(number);
+            Messeage =  $" Fiboonachi({number}):" + fiboonachi.result ;
+        }
+    } 
+  
+}
---
+++ infrastructure/Fiboo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure
+{
+    public class Fiboo
+    {
+        public int result;
+        public Fiboo(int number)
+        {
+            result = GetFiboo(number);
+        }
+        public int GetFiboo(int number)   
+        {
+            if (number == 0) return 0;
+            if (number == 1) return 1;
+            return GetFiboo(number-1) + GetFiboo(number-2);
+
+        }   
+    }
+}
